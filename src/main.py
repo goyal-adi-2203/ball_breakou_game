50444fda5552803717b2d805aa358dc969b7160a
goyal-adi-2203 <114351348+goyal-adi-2203@users.noreply.github.com>
2022-11-15 01:55:22 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,37 @@
 
 # turtle module for using console window
 import turtle as t
+import time
 
-# creating screen background
+# importing classes 
+# PADDLE 
+from pdle import Paddle 
+
+
+# creating console background
 scr = t.Screen()
 scr.setup(width=1200,height=600)
 scr.bgcolor('black')
 scr.title('BREAKOUT')
+scr.tracer(0)
 
+
+# instances of classes
+# PADDLE
+pd=Paddle()
+
+
+
+scr.listen()
+
+# defining which keys it needs to listen to, 
+# which are the left and right arrow keys (to 
+# move the paddle) , SPACE bar (to pause/resume 
+# the game) and R to restart.
+scr.onkey(key='Left',fun=pd.left)
+scr.onkey(key='Right',fun=pd.right)
+
+
+scr.update()
 
 t.mainloop()
