507b17cf940c1b6e849494a3c5f9546dd39fc35d
goyal-adi-2203 <114351348+goyal-adi-2203@users.noreply.github.com>
2022-11-15 15:11:22 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,17 +21,38 @@ scr.tracer(0)
 # PADDLE
 pd=Paddle()
 
+# initialising game
+game_pause = False
+play_game = True
+
+
+# function to stop the game
+def restart():
+    global play_game
+    play_game = False
+    time.sleep(1)
+    scr.bye()
 
 
 scr.listen()
 
 # defining which keys it needs to listen to, 
 # which are the left and right arrow keys (to 
-# move the paddle) 
-scr.onkey(key='Left',fun=pd.left)
-scr.onkey(key='Right',fun=pd.right)
+# move the paddle) and R to stop.
+scr.onkey(key='Left',fun = pd.left)
+scr.onkey(key='Right',fun = pd.right)
+scr.onkey(key='r',fun = restart)
 
 
-scr.update()
+# starting the game
+while play_game:
+
+    time.sleep(0.02)    
+    # playing if game not paused
+    if not game_pause:
+        # updating console
+        scr.update()
+        time.sleep(0.01)
+
 
 t.mainloop()
