cd12d18b4d6c1df88c7bfd816f3a5ef4c06b4622
goyal-adi-2203 <114351348+goyal-adi-2203@users.noreply.github.com>
2022-11-16 21:57:49 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,6 +13,9 @@ from ui import UI
 from bricks import Bricks
 # scoreboard
 from scrbrd import Scoreboard
+# BALL 
+from ball import Ball
+
 
 # creating console background
 scr = t.Screen()
@@ -31,6 +34,10 @@ ui = UI()
 bricks = Bricks()
 # scoreboard
 score = Scoreboard(lives = 3)
+# BALL
+bl=Ball()
+
+
 
 # calling header function to write heading
 ui.header()
@@ -39,24 +46,32 @@ ui.header()
 game_pause = False
 play_game = True
 
+# function to pause the game
+def pause_game():
+    global game_pause
 
+    if game_pause:
+        game_pause = False
+    else:
+        game_pause = True
+    
+    
 # function to end game
 def end_game():
     global play_game
     play_game = False
     scr.bye()
 
-
 # function to restart the game
 def restart():
-    global game_pause,play_game
+    global game_pause,play_game,bl,score,pd,bricks
+    game_pause = False 
     play_game = True
-    game_pause = False
-    pd.reset()
+    bl.reset()
     score.reset()
     bricks.reset()
+    pd.reset()
     time.sleep(0.5)
-    # scr.bye()
 
 
 scr.listen()
@@ -68,7 +83,39 @@ scr.onkey(key='Left',fun = pd.left)
 scr.onkey(key='Right',fun = pd.right)
 scr.onkey(key='r',fun = restart)
 scr.onkey(key='s',fun = end_game)
+scr.onkey(key='space',fun = pause_game)
 
+
+
+# CHECKING THE COLLISIONS OF BALL 
+# collision with WALLS
+def check_coll_walls():
+
+    global bl,score,play_game,ui
+
+    # collision with left and right walls
+    if bl.xcor() < -580 or bl.xcor() > 570:
+        bl.bounce(x_bounce=True,y_bounce=False)
+        return
+
+    # collision with upper wall
+    if bl.ycor() > 270 :
+        bl.bounce(x_bounce=False,y_bounce=True)
+        return
+
+    # collision with lower wall, 
+    # in which case user fails and game resets
+    if bl.ycor() < -280:
+        bl.reset()
+        score.dec_lvs()
+
+        # GAME OVER and uses lost if lives = 0
+        if score.lvs == 0:
+            score.reset()
+            play_game = False
+            ui.GO(W=False)
+            return
+        return
 
 
 # starting the game
