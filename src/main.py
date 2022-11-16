25e57ceabf4dcbf5ce4ea27b5bc2fee6be9dd6f3
goyal-adi-2203 <114351348+goyal-adi-2203@users.noreply.github.com>
2022-11-16 23:03:37 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ from scrbrd import Scoreboard
 # BALL 
 from ball import Ball
 
+TEMP = False
 
 # creating console background
 scr = t.Screen()
@@ -48,30 +49,18 @@ play_game = True
 
 # function to pause the game
 def pause_game():
-    global game_pause
-
+    global game_pause,ui
     if game_pause:
+        # ui.header()
         game_pause = False
     else:
         game_pause = True
     
     
 # function to end game
-def end_game():
+def stop():
     global play_game
     play_game = False
-    scr.bye()
-
-# function to restart the game
-def restart():
-    global game_pause,play_game,bl,score,pd,bricks
-    game_pause = False 
-    play_game = True
-    bl.reset()
-    score.reset()
-    bricks.reset()
-    pd.reset()
-    time.sleep(0.5)
 
 
 scr.listen()
@@ -81,14 +70,14 @@ scr.listen()
 # move the paddle) s to stop and R to restart.
 scr.onkey(key='Left',fun = pd.left)
 scr.onkey(key='Right',fun = pd.right)
-scr.onkey(key='r',fun = restart)
-scr.onkey(key='s',fun = end_game)
+scr.onkey(key='s',fun = stop)
 scr.onkey(key='space',fun = pause_game)
 
 
 
 # CHECKING THE COLLISIONS OF BALL 
-# collision with WALLS
+
+# WALLS
 def check_coll_walls():
 
     global bl,score,play_game,ui
@@ -99,7 +88,7 @@ def check_coll_walls():
         return
 
     # collision with upper wall
-    if bl.ycor() > 270 :
+    if bl.ycor() > 240 :
         bl.bounce(x_bounce=False,y_bounce=True)
         return
 
@@ -112,21 +101,127 @@ def check_coll_walls():
         # GAME OVER and uses lost if lives = 0
         if score.lvs == 0:
             score.reset()
-            play_game = False
             ui.GO(W=False)
+            time.sleep(2)
+            play_game = False
             return
         return
 
 
+# PADDLE
+def check_coll_paddle():
+    
+    global bl,pd
+
+    # x coordinates of paddle and ball
+    pd_x = pd.xcor()
+    bl_x = bl.xcor()
+
+    # checking for coll
+    # by using centre of ball and paddle
+    if bl.distance(pd) < 110 and bl.ycor() < -250 :
+        
+        # when paddle is right of scr
+        if pd_x > 0:
+            if bl_x > pd_x:
+                # balls hits left side
+                bl.bounce(x_bounce=True,y_bounce=True)
+                return
+            
+            else:
+                bl.bounce(x_bounce=False,y_bounce=True)
+                return
+        
+        # when paddle is left of scr
+        elif pd_x < 0 :
+            if bl_x < pd_x:
+                # balls hits left side
+                bl.bounce(x_bounce=True,y_bounce=True)
+                return
+            
+            else:
+                bl.bounce(x_bounce=False,y_bounce=True)
+                return
+        
+        # paddle in centre
+        else:
+            if bl_x == pd_x:
+                bl.bounce(x_bounce=False,y_bounce=True)
+                return
+            else:
+                bl.bounce(x_bounce=True,y_bounce=True)
+                return
+
+# BRICKS
+def check_coll_brick():
+
+    global bl,bricks,score
+
+
+    for brick in bricks.brks:
+
+        # when ball in range
+        if bl.distance(brick) < 40: 
+            
+            brick.clear()
+            brick.goto(3000,3000)
+            bricks.brks.remove(brick)
+            
+            # detection coll to bounce the ball
+            # LEFT 
+            if bl.xcor() < brick.left_wall:
+                bl.bounce(x_bounce=True,y_bounce=False)
+                
+            # RIGHT
+            elif bl.xcor() > brick.right_wall:
+                bl.bounce(x_bounce=True,y_bounce=False)
+            
+            # BOTTOM 
+            elif bl.ycor() < brick.bottom_wall:
+                bl.bounce(x_bounce=False,y_bounce=True)
+            
+            # TOP
+            elif bl.ycor() > brick.upper_wall:
+                bl.bounce(x_bounce=False,y_bounce=True)
+
+            # increasing score
+            score.inc_score()
+
+
+time.sleep(1)
 # starting the game
 while play_game:
 
-    time.sleep(0.02)    
     # playing if game not paused
     if not game_pause:
+        
+        if TEMP:
+            TEMP = False
+            ui.header()
+
         # updating console
         scr.update()
-        time.sleep(0.01)
+        time.sleep(0.02)      
+        bl.move()
+
+        # checking collisions with walls
+        check_coll_walls()
+
+        # checking collision with paddle
+        check_coll_paddle()
     
+        # checking collisions with bricks
+        check_coll_brick()
+        
+        # checking for win situation
+        if len(bricks.brks) == 0:
+            ui.GO(W=True)
+            break
+    
+    else:
+        TEMP = True
+        ui.when_pause(score.score)
+    
+
 scr.bye()
 t.mainloop()
